0216a32db8d31ed51ea6c36c7f8f95e3d58e38fb
LN-RA <helenegaumont2@gmail.com>
2025-09-05 11:18:15 +0200

    Update main.py

--- wolfactiv_mbti_api/main.py
+++ wolfactiv_mbti_api/main.py
@@ -1,5 +1,5 @@
 # --- imports ---
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException, Request
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 from dotenv import load_dotenv
